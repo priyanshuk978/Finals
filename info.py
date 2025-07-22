df798ec3be821e3fb704dc8f52c22231f9f9ff34
priyanshuk978 <priyanshukumawat2k25@gmail.com>
2025-07-22 16:33:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,10 +88,10 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', '')
-CHNL_LNK = environ.get('CHNL_LNK', '')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', '')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/moviesearchgroupmovixnest')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/MovixNest')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MovixNest_ownerbot') # Support Chat Link Without https:// or @
+OWNER_LNK = environ.get('OWNER_LNK', 'http://t.me/MovixNest_ownerbot')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
